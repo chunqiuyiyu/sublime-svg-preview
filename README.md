b6c5b8cff1618f6c81093fa72e07098fd8cba1d5
root <root@DESKTOP-0UI6JD4.localdomain>
2020-06-01 23:25:03 +0800

    Init repo

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# sublime-svg-preview
+# Sublime SVG Preview
+
+> Sublime Text plugin to preview SVG files.
+
+**Important tip: You must install [Inkscape](https://inkscape.org/) before you use this plugin, this plugin only works with Sublime Text 3.**
+
+![Preview](/preview.png)
+
+## Install
+
+### Package Control
+
+Recommand reach `Sublime SVG Preview` via [Package Control](https://packagecontrol.io/packages/Sublime-SVG-Preview).
+
+### Manually
+
+1. Locate your Sublime Text Packages directory by using Sublime Text menu `Preferences` -> `Browse Packages....`.
+2. Cloning the repository directly into your Sublime Text Packages directory.
+```bash
+git clone https://github.com/chunqiuyiyu/sublime-svg-preview svg-Preview
+```
+
+## Usage
+
+Just right-click and select `SVG Preview` in contextmenu when you view SVG file.
+
+If you want to use hotkey, open menu `Preferences` -> `Key Bindings` and add key bindings like these:
+
+```json
+{
+  "keys": ["ctrl+shift+v"],
+  "command": "svg_preview"
+}
+``` 
+## License
+
+[MIT](/LICENSE)
--- utils.py
+++ utils.py
@@ -1,1 +1,60 @@
+"""
+Utility functions for current plugin
+"""
 
+import os
+import tempfile
+
+import sublime
+
+TMP_DIR = os.path.join(tempfile.gettempdir(), 'sublime-svg-preview')
+PLUGIN_NAME = 'Sublime SVG Preview'
+
+
+def keep_tmp_pool():
+    """
+    Delete oldest file in tmp directory when files have exceed
+    """
+    if bool(len(os.listdir(TMP_DIR)) > 20):
+        # We only keep 10 SVG files in tmp directory, So max size is 20
+        # (10 original SVG files and 10 automatically generated PNG files)
+
+        all_files = os.listdir(TMP_DIR)
+
+        file_mtimes = [
+            os.stat(os.path.join(TMP_DIR, name)).st_mtime for name in all_files
+        ]
+        index = file_mtimes.index(min(file_mtimes))
+        oldest_file = all_files[index]
+        print(oldest_file)
+
+        os.remove(os.path.join(TMP_DIR, oldest_file))
+
+
+def active_console(err_msg, cmd):
+    """
+    Active console panel when some errors occurs.
+    """
+    msg = '{}: {}\nRun `{}` in your bash for information.'.format(
+        PLUGIN_NAME, err_msg, cmd)
+
+    sublime.active_window().run_command("show_panel", {"panel": "console"})
+    raise Exception(msg)
+
+
+def run_cmd(cmd, msg):
+    """
+    Run custom command and deal with std errors
+    """
+    stdout = os.popen(cmd)
+
+    result = stdout.buffer.read().decode(encoding='utf8')
+    stderr = stdout.close()
+    return result if not stderr else active_console(msg, cmd)
+
+
+def get_origin_name(path):
+    """
+    Getting the name of the file without the extension
+    """
+    return os.path.splitext(path)[0]
